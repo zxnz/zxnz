17777766bb285e37615e5f12029c104dc8e768ce
yangyxu <yangyxu@cisco.com>
2021-05-23 09:42:14 +0800

    update deps

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 require('./src/zxnz');
 
-if(process.env.NODE_ENV == 'development'){
-    var node_path = require('path');
-    var node_fs = require('fs');
-    var _version = '';
-    if(node_fs.existsSync(node_path.join(__dirname, './package.json'))){
-        _version = require(node_path.join(__dirname, './package.json')).version;
-    }
-    zn.debug('zxnz_path', (_version?'[ ' + _version + ' ]':''), ': ', __dirname);
+var node_path = require('path');
+var node_fs = require('fs');
+var _version = '';
+if(node_fs.existsSync(node_path.join(__dirname, './package.json'))){
+    _version = require(node_path.join(__dirname, './package.json')).version;
 }
+zn.info('zxnz_path', (_version?'[ ' + _version + ' ]':''), ': ', __dirname);
+
 module.exports = zn.extend(zxnz, {
     entity: require('./src/entity/index.js'),
     ref: require('./src/ref/index.js')
