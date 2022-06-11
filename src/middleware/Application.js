a4c4e1285fc091ec02cae638f35472ad52cfa3fa
yangyxu <yangyxu@cisco.com>
2022-06-11 12:43:19 +0800

    rebuild

--- src/middleware/Application.js
+++ src/middleware/Application.js
@@ -13,17 +13,19 @@ module.exports = zn.Middleware.Application({
         initControllers: function (application, controllers){
             var _config = zn.extend({}, application._config, application._serverContext._config),
                 _mode = _config.mode || process.env.NODE_ENV;
-            /*
+            zn.debug('mode: ', _mode)
             if(_mode == 'development'){
                 Controllers.forEach(function (Controller){
                     controllers[Controller.getMeta('controller')] = Controller;
                 });
-            }*/
+            }
+            /*
             Controllers.forEach(function (Controller){
                 if(Controller && Controller.getMeta('controller')) {
                     controllers[Controller.getMeta('controller')] = Controller;
                 }
             });
+            */
         },
         modelLoaded: function (key, model, application, serverContext){
             model.createModelSql(key, application, serverContext);
